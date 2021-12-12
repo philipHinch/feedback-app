96b6d1743492246805669e377b12bdbdc5196fd2
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-12-12 21:36:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './components/context/FeedbackContext'
+
+function App() {
+
+  return (
+    <FeedbackProvider >
+      <Router>
+        <Header />
+        <div className='container'>
+          <Route exact path='/'>
+            <FeedbackForm />
+            <FeedbackStats />
+            <FeedbackList />
+          </Route>
+
+          <Route path='/about' component={AboutPage} />
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
+}
+
+export default App
